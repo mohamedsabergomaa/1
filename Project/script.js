468970190fc74f26da01bae1c732aeabee76c2cb
mohamedsabergomaa <144049182+mohamedsabergomaa@users.noreply.github.com>
2023-09-21 15:39:41 +0300

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,21 @@
+let user = {
+    age : 35,
+    doubleAge : function () {
+        return this.age * 2 ;
+    },  
+};
+console.log (user) 
+console.log (user.age)
+console.log (user.doubleAge())
+let obj= Object.create({});
+
+
+obj.a = 100 ;
+console.log(obj)
+let copyObj= Object.create(user);
+
+copyObj.age = 30 ;
+
+console.log (copyObj) 
+console.log (copyObj.age)
+console.log (copyObj.doubleAge())
